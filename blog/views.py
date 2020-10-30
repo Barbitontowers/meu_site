9d0d6345e68108a63e1810e473c7837efd937da0
Barbiton <bar_biton@outlook.com>
2020-10-29 22:26:29 -0300

    Meu primeiro commit

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,21 @@
+from django.shortcuts import render
 from django.views.generic import DetailView, ListView
-
+from django.views.generic.edit import CreateView
 from .models import Post
 
 
 class PostListView(ListView):
     model = Post
-    
+    template_name = 'post/home.html' 
 
 
 class PostDetailView(DetailView):
     model = Post
+  #  template_name = 'post/detail.html'
+
+class PostCreatelView(CreateView):
+    model = Post
+#    template_name = 'post/post_new.html'
+#    fields = '__all__'
+
+
